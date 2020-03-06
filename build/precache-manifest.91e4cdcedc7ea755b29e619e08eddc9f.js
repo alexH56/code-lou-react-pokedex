a143fcf2dd4994ce15b1859b61fceb398d94563b
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-03-05 21:57:08 -0500

    Added build directory for deployment

---
+++ build/precache-manifest.91e4cdcedc7ea755b29e619e08eddc9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0aceacfdecbf51961ba5545ad95517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5df529a37606f00e5949",
+    "url": "/static/css/main.3f127949.chunk.css"
+  },
+  {
+    "revision": "650c94849c373a60abef",
+    "url": "/static/js/2.a8812194.chunk.js"
+  },
+  {
+    "revision": "5df529a37606f00e5949",
+    "url": "/static/js/main.e91486ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
